06a591d6344f43ac056e8a9ff152b39c99fb68cc
Roman Boiko <roma685235423@icloud.com>
2024-02-12 23:38:59 +0300

    Calculator VIew, Model and ViewModel implemented

--- MoexSUICalculator/MoexSUICalculator/MoexSUICalculatorApp.swift
+++ MoexSUICalculator/MoexSUICalculator/MoexSUICalculatorApp.swift
@@ -1,17 +1,10 @@
-//
-//  MoexSUICalculatorApp.swift
-//  MoexSUICalculator
-//
-//  Created by Роман Бойко on 12.02.2024.
-//
-
 import SwiftUI
 
 @main
 struct MoexSUICalculatorApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CalculatorView(viewModel: CalculatorViewModel())
         }
     }
 }
