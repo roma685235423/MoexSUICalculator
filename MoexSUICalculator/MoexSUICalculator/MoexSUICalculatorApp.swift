e0b6d21c964208c983f05d80d20c700cbf5306d3
Roman Boiko <roma685235423@icloud.com>
2024-02-12 19:13:21 +0300

    Progect done

---
+++ MoexSUICalculator/MoexSUICalculator/MoexSUICalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoexSUICalculatorApp.swift
+//  MoexSUICalculator
+//
+//  Created by Роман Бойко on 12.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct MoexSUICalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
